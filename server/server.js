048fe83cccbd921043db06d355c93a8b21c454ff
myadavaramshravan-ship-it <myadavaramshravan@gmail.com>
2026-07-27 16:05:24 +0530

    fix vercel react routing

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://YOUR-VERCEL-APP.vercel.app",
+      "https://prodesk-it-client-project-5.vercel.app/login",
     ],
     credentials: true,
   })
